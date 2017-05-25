b3a9d7d81e06e4e5979ea4f97391ab112977ba8b
Ishibori <JIshibori@gmail.com>
2017-05-25 20:35:40 +0200

    Added survey models

---
+++ app/src/main/java/SurveyModels/SurveyOption.java
@@ -0,0 +1,17 @@
+package SurveyModels;
+
+import android.graphics.Bitmap;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Ishibori on 28/01/2017.
+ */
+
+public class SurveyOption {
+    int order;
+    String description;
+    Bitmap image;
+    // sound
+    // video
+}
---
+++ app/src/main/java/SurveyModels/TargetGroup.java
@@ -0,0 +1,8 @@
+package SurveyModels;
+
+/**
+ * Created by Ishibori on 28/01/2017.
+ */
+
+public class TargetGroup {
+}
